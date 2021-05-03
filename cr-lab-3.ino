767086cece11725db06f7d5c03722271d9bb7cf2
Mustafa Darwesh <mustafa@Mustafas-MacBook-Pro.local>
2021-05-03 15:50:42 +0200

    Implement V1

---
+++ cr-lab-3.ino
@@ -0,0 +1,70 @@
+// Access the required libraries:
+#include <MeMCore.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+MeDCMotor rightMotor(9); 
+MeDCMotor leftMotor(10);
+
+MeLineFollower linefollower(2);
+
+void applyPower(int power, int motor){
+  power *= 2.55;
+  switch(motor){
+    case 0:
+      rightMotor.run(power);
+      leftMotor.run(-power);
+      break;
+    case 1:
+      leftMotor.run(-power);
+      break;
+    case 2:
+      rightMotor.run(power);
+      break;
+      
+  }
+}
+
+int speed = 50;
+
+void goRight(){
+  Serial.print("Going Right");
+  applyPower(speed, 2);
+}
+
+void goLeft(){
+  Serial.print("Going Left");
+  applyPower(speed, 1);
+}
+
+void goForward(){
+  Serial.print("Going Forward");
+  applyPower(speed, 0);
+}
+
+void goBack(){
+  Serial.print("Going Back");
+  applyPower(-speed, 0);
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorstate = linefollower.readSensors();
+  switch(sensorstate){
+    case 0:
+      goBack();
+      break;
+    case 1:
+      goLeft();
+      break;
+    case 2:
+      goRight();
+      break;
+    case 3:
+      goForward();
+  }
+}
